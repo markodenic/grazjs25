33678cb7e7e8cb99e9339f1fd6e3f805595c5375
Marko Denic <marko.denic@web-ideenreich.at>
2018-12-11 10:35:36 +0100

    About page content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="about">
-      <h1>This is an about page.</h1>
-  </div>
+  <el-row type="flex" justify="center" class="about">
+      <el-col :xs="22" :sm="10">
+          <h1>GrazJs #25</h1>
+
+          <h2>"VueJS", by Marko Denić</h2>
+
+          <h3>Presentation slides:</h3>
+
+          <a href="https://docs.google.com/presentation/d/1vItGIyJ3TkTDv0qZS5emJC4Uvl8pcwDZeGO4cz-mDMw/edit?usp=sharing">https://docs.google.com/presentation/d/1vItGIyJ3TkTDv0qZS5emJC4Uvl8pcwDZeGO4cz-mDMw/edit?usp=sharing</a><br>
+
+          <h3>Presentation code:</h3>
+
+          <a href="https://github.com/MarkoDenic/grazjs25">https://github.com/MarkoDenic/grazjs25</a>
+
+          <h3>Social:</h3>
+
+          <p>
+              <strong>Linkedin:</strong> <a href="https://www.linkedin.com/in/marko-denic-75b3b0a4/">https://www.linkedin.com/in/marko-denic-75b3b0a4/</a><br>
+              <strong>Xing:</strong> <a href="https://www.xing.com/profile/Marko_Denic3">https://www.xing.com/profile/Marko_Denic3</a> <br>
+              <strong>Twitter:</strong> <a href="https://twitter.com/denicmarko">https://twitter.com/denicmarko</a><br>
+          </p>
+      </el-col>
+  </el-row>
 </template>
+
+<style lang="less">
+    .about {
+        text-align: left;
+        h1, h2 {
+            text-align: center;
+        }
+        a {
+            color: #7ec5bf;
+            -webkit-transition: all .2s;
+            transition: all .2s;
+            &:hover {
+                color: lighten(#7ec5bf, 10%);
+            }
+        }
+    }
+</style>
